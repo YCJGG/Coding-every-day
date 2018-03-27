120e86b3c36a1ba7c57394ab67f393fe4dad7bef
YCJGG <251623266@qq.com>
2018-03-27 11:44:20 +0800

    1#4

---
+++ Leetcode/14_LongestCommonPrefix.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+        flag = True
+        prefix = min(strs, key = len)
+        for i ,ch in enumerate(prefix):
+            for other in strs:
+                if other[i] != ch:
+                    return prefix[:i] 
+        return prefix
+        # for i in range(0,len(strs)):
+            
+        #     for k in range(len(prefix)):
+        #         if prefix[k] != strs[i][k]:
+        #             flag = False
+        #     if flag == False:
+        #         prefix = prefix[:-1]
+        #     print(i,prefix)
+        # return prefix
+                    
+strs = ["flower","flow","flight"]
+print(Solution().longestCommonPrefix(strs))
